b9f805dcfc2ae58416a3d4e3ce45bd63586bebfe
范端端 <1208674494@qq.com>
2019-03-14 21:02:12 +0800

    Updates

---
+++ precache-manifest.f18794650cd4cd2c728d327ab14264ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e02f8964d4c5bcf5e2",
+    "url": "/react-pwa-page/static/css/main.72de9e8f.chunk.css"
+  },
+  {
+    "revision": "d8e02f8964d4c5bcf5e2",
+    "url": "/react-pwa-page/static/js/main.05c6fe3e.chunk.js"
+  },
+  {
+    "revision": "42dc3f4e9ef496851e3e",
+    "url": "/react-pwa-page/static/js/runtime~main.284f3de8.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-pwa-page/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa-page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95eff68991fa7417dca50f49215272c1",
+    "url": "/react-pwa-page/index.html"
+  }
+];
