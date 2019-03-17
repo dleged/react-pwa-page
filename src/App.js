8c4cb5a65da4cbbcbda0a9956cdfd097a4e0f8f2
一木 <1208674494@qq.com>
2019-03-17 20:57:38 +0800

    🐬

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import httpRequest from './utils/request.js';
 
 class App extends Component {
   state = {
     contents: ''
   }
 
-  httpRequest = (url,callback) => {
-    let xhr = this.hre || new XMLHttpRequest();
-    xhr.onload = callback;
-    xhr.open('get',url);
-    xhr.send();
-  }
-
   fetchUrl = (url) =>{
-    return new Promise((res) => {
-      this.httpRequest(url,function(responseText){
-        let target = responseText.target;
-        if(target.readyState === 4 && target.status === 200){
-          res(target)
-        }
-      })
-    })
+  	return new Promise((res) => {
+  		httpRequest(url,function(responseText){
+  			let target = responseText.target;
+  			if(target.readyState === 4 && target.status === 200){
+  				res(target)
+  			}
+  		})
+  	})
   }
 
   componentDidMount() {
+    this.fetchUrl('www.npmjs.com/package/npm-run-all').then((responseText) => {
+      this.setState({
+        contents: responseText.responseText
+      })
+    });
+  }
+
+  componentWillMount() {
     this.fetchUrl('www.npmjs.com/package/npm-run-all').then((responseText) => {
       this.setState({
         contents: responseText.responseText
@@ -50,9 +52,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
-        <div>
+        <pre>
           {this.state.contents}
-        </div>
+        </pre>
       </div>
     );
   }
---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+let num = num || 1;
+
+const httpRequest = (url,callback) => {
+	let xhr = new XMLHttpRequest();
+	xhr.onload = callback;
+	xhr.open('get',url);
+	xhr.send();
+	num ++;
+	console.log(num,'num');
+	window['xhr' + Math.floor(Math.random(0,1)*100)] = xhr;
+}
+
+export default httpRequest;
