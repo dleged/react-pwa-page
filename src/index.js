726915213bc67f5f84e2bfa91ccc0c467775823d
一木 <1208674494@qq.com>
2019-04-01 22:25:20 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+// window.zj = React.createElement('div','','浙江');
+// let hz = React.createElement('div','','杭州');
+// window.reactDOM = React.createElement('h1',{text:'爱我中华🇨🇳'},zj,hz);
+
+
+class Hello extends React.Component{
+
+	state = {
+		num: 0
+	}
+
+	componentDidMount(){
+		window.hello = this;
+		var i = 0;
+		setInterval(() => {
+			this.setState({
+				num: ++i
+			},function(){
+				console.trace('runtime react state!')
+			})
+		},3000)
+	}
+
+
+	render(){
+		return <h2>hello china 🇨🇳,{this.state.num}</h2>
+	}
+}
+
+function App(props){
+	return <div>{props.txt}</div>
+}
+
 ReactDOM.render(
-	<App />,
+	<Hello />,
 	document.getElementById('root')
 );
-document.addEventListener("DOMContentLoaded", function(event) {
-		console.log("DOM fully loaded and parsed");
-});
-
-for(var i=0; i<10000000000; i++){
-		// 这个同步脚本将延迟DOM的解析。
-		// 所以DOMContentLoaded事件稍后将启动。
-}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
